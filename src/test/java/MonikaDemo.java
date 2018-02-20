a0b802d16942daef7bc1d78fa4b85ba662cb7ee1
KarimGitHub <karim.fan4@gmail.com>
2018-02-20 22:30:46 +0530

    Push all java and others

---
+++ src/test/java/MonikaDemo.java
@@ -0,0 +1,62 @@
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import wrappers.GenericWrappers;
+
+
+public class MonikaDemo extends GenericWrappers {
+
+	public static void main(String[] args) {
+		
+		/*
+		//Launch the browser
+		
+		//exe file 
+		
+		System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
+		
+		//Syntax to create object
+		// ClassName objectName = new ClassName();
+		ChromeDriver obj = new ChromeDriver();
+		
+		obj.get("https://www.linkedin.com/");*/
+		
+		// all conditional statement
+		
+		int  a = 20;
+		
+		if(a==10){
+			System.out.println("IF");
+		}else if(a==15){
+			System.out.println("Else if");
+		}
+
+	}
+	
+	
+	
+	public void doLogin(String url, String userName, String password){
+		
+		
+	}
+	
+	
+	
+	
+	
+	public void doLogin(int url, String userName, String password){
+		
+	}
+	
+	
+	/*public void doLogin(String userName, String password, String otp){
+		
+	}*/
+	
+	
+	
+	
+	
+	
+	
+
+}
---
+++ src/test/java/testcases/RunScripts.java
@@ -0,0 +1,57 @@
+package testcases;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import dataproviders.CallWrappersUsingKeyword;
+import wrappers.LinkedInWrappers;
+
+public class RunScripts extends LinkedInWrappers{
+
+
+
+	@Test
+	public void runScripts() throws IOException {
+
+		CallWrappersUsingKeyword keywords = new CallWrappersUsingKeyword();
+
+		try {
+			FileInputStream fis = new FileInputStream("./keywords/KeywordDriver.xlsx");
+			XSSFWorkbook workbook = new XSSFWorkbook(fis);
+			XSSFSheet sheet = workbook.getSheetAt(0);	
+
+			// get the number of rows
+			int rowCount = sheet.getLastRowNum();			
+
+			// loop through the rows
+			for(int i=1; i <rowCount+1; i++){
+				try {
+					XSSFRow row = sheet.getRow(i);
+					
+					if(row.getCell(3).getStringCellValue().toLowerCase().equals("yes")){
+						
+						keywords.getAndCallKeyword("./keywords/"+row.getCell(1).getStringCellValue()+".xlsx");
+					}
+
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+			fis.close();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+
+
+
+	}
+
+}
